8ca36296f5a09081ffb762b8fafea753f67dbb89
Victor Holz <victormholz@gmail.com>
2021-05-13 22:52:56 -0300

    Add files via upload

---
+++ ex028.py
@@ -0,0 +1,11 @@
+from random import randint
+from time import sleep
+computador = randint(0, 5)
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+jogador = int(input('Em que número eu pensei? '))
+print('PROCESSANDO...')
+sleep(3)
+if jogador == computador:
+    print('Parabéns você conseguiu me vencer!')
---
+++ ex030.py
@@ -0,0 +1,6 @@
+número = int(input('Me diga um número qualquer: '))
+resultado = número % 2
+if resultado == 0:
+    print(f'O número {número} é par.')
+else:
+    print(f'O número {número} é ímpar.')
